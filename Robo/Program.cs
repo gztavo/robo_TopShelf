bd16d58c320d03fc4e58ab423996c2c28e70709b
Gustavo Costa Rosa <gcrosa@latam.stefanini.com>
2018-10-22 16:37:15 -0300

    Robo, TOPSHELF

---
+++ Robo/Program.cs
@@ -0,0 +1,13 @@
+﻿using Robo;
+using System;
+
+namespace Robô
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureService.Configure();
+        }
+    }
+}
